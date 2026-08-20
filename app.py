e9903deba658e54c7b507cf46a47b883ef672b3b
ANCPC <166911287+ANCPC@users.noreply.github.com>
2026-08-20 16:47:14 +0000

    Force cloud redeployment and added removed port

--- app.py
+++ app.py
@@ -167,4 +167,7 @@ def generate():
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    # Render binds port dynamically via environment variables
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
